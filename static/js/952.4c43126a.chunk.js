3bb163f983a153f8d4fe8d80ffac2d006ca0b73d
arielle <arielle95@gmail.com>
2025-01-19 17:01:19 -0300

    Updates

---
+++ static/js/952.4c43126a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktcc_arielle=self.webpackChunktcc_arielle||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.4c43126a.chunk.js.map
